5a9135a003e29e38fad701b118cbd0abbb80d619
Tashi Dolma <tadolma97@gmail.com>
2022-08-11 14:08:17 -0400

    commit tree 97d88383fde9a8309183709a7bdf30cfd180795a

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,22 @@ class LinkedList
 
   def nth_from_end(n)
     # your code here
+    return nil if n<1
+    counter=0
+    target=head 
+    while target 
+      counter+=1
+      target=target.next_node
+    end
+    return nil if n<1 || n>counter
+    i=1
+    node=head
+    while i<counter-n+1
+      node=node.next_node 
+      i+=1
+    end
+    node.value
   end
+
 
 end
